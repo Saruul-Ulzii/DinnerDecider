fa43cca7c076228f3b8383be4c089427b724692a
Saruul <s.saruululzii@gmail.com>
2022-10-23 21:56:51 -0400

    Updates

--- app/src/main/java/com/sw06d120/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/sw06d120/dinnerdecider/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.sw06d120.dinnerdecider
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var foods: ArrayList<String> = ArrayList(arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_add_food.setOnClickListener{
+            lbl_food_name.setText(txt_new_name.text)
+            foods.add(txt_new_name.text.toString())
+            txt_new_name.setText("")
+        }
+
+        btn_decide.setOnClickListener {
+            lbl_food_name.setText(foods.random())
+        }
     }
 }
