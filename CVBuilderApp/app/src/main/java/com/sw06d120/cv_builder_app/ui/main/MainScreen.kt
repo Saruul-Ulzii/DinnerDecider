c27c70dbe0a808c524e61bf500d2265cd0fe4f86
Saruul <s.saruululzii@gmail.com>
2022-12-12 23:23:56 -0500

    Updates

--- CVBuilderApp/app/src/main/java/com/sw06d120/cv_builder_app/ui/main/MainScreen.kt
+++ CVBuilderApp/app/src/main/java/com/sw06d120/cv_builder_app/ui/main/MainScreen.kt
@@ -1,7 +1,14 @@
 package com.sw06d120.cv_builder_app.ui.main
 
 import ViewPagerAdapter
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
@@ -45,5 +52,47 @@ class MainScreen : AppCompatActivity() {
         tab.getTabAt(1)?.setIcon(tabIcons[1]);
         tab.getTabAt(2)?.setIcon(tabIcons[2]);
         tab.getTabAt(3)?.setIcon(tabIcons[3]);
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @SuppressLint("IntentReset")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.linkedin -> {
+                try {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://www.linkedin.com/in/saruululzii-sampilnorov/")
+                    intent.setPackage("com.linkedin.android")
+                    startActivity(intent)
+                } catch (anfe: ActivityNotFoundException) {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("https://www.linkedin.com/in/saruululzii-sampilnorov/")
+                        )
+                    )
+                }
+            }
+            R.id.contact -> {
+                val mIntent = Intent(Intent.ACTION_SEND)
+                Uri.parse("mailto:").also { mIntent.data = it }
+                mIntent.type = "text/plain"
+                mIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("s.saruululzii@gmail.com"))
+                mIntent.putExtra(Intent.EXTRA_SUBJECT, "Resume app")
+                mIntent.putExtra(Intent.EXTRA_TEXT, "Greeting from Resume app")
+
+                try {
+                    startActivity(Intent.createChooser(mIntent, "Send in ..."))
+                } catch (e: Exception) {
+                    Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
