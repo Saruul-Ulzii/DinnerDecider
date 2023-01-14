28e045d679186d69d76f33ee162b09fbaa603c17
Saruul <s.saruululzii@gmail.com>
2023-01-14 14:38:17 -0500

    QuizApp - Init commit

---
+++ QuizApp/app/src/main/java/com/sw06d120/miuquiz/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sw06d120.miuquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sw06d120.miuquiz.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
